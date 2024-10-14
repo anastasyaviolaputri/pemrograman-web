f993da8f00e252dda0450bd2fce63accd0b7ead8
anastasyaviolaputri <anastasyaviola17@gmail.com>
2024-10-14 11:26:55 +0700

    tugas

---
+++ Pertemuan2/variabel HTML.php
@@ -0,0 +1,16 @@
+<?php
+$nama = "Anastasya Viola Putri";
+$umur = "19";
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Contoh Variabel PHP</title>
+</head>
+<body>
+    <h1>Halo, <?php echo $nama; ?>!<h1>
+    <p>Umur Anda Adalah <?php echo $umur; ?> tahun.</p>
+</body>
+</html> 
---
+++ Pertemuan2/variabel.php
@@ -0,0 +1,7 @@
+<?php
+$nama = "Anastasya Viola Putri";
+$umur = "19";
+
+echo"$nama <br>";
+echo"$umur";
+?>
